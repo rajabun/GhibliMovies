db42ee3e81cfff8a02fe71950ac767911fc21b6a
Muhammad Rajab <ichijo_rajab@hotmail.com>
2021-04-19 22:59:10 +0700

    Initial Commit

---
+++ GhibliMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GhibliMovies
+//
+//  Created by Muhammad Rajab Priharsanto on 19/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
